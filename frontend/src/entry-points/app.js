c5448aaef7505f77efb4bc0b32ade214563d3041
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-07-20 20:49:07 +0200

    PB-2 : Mise en forme

--- frontend/src/entry-points/app.js
+++ frontend/src/entry-points/app.js
@@ -1,3 +1,7 @@
+import 'tom-select/dist/css/tom-select.min.css';
 import '../../assets/app.css';
+import '../../assets/bootstrap.scss'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import '../script/onLoad'
---
+++ frontend/src/script/onLoad.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', onLoad, false);
+
+import {initializeSelects} from '../plugins/select.js';
+
+function onLoad () {
+    initializeSelects();
+}
