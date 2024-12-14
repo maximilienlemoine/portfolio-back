5f8eb2b3e93f0c39ff32acb3539aa2144c8d8e8d
Maximilien LEMOINE <maximilien.lemoine.pro@gmail.com>
2024-12-14 17:01:30 +0100

    feat : monitoring

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
