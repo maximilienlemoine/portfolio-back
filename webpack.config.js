230fd82e90304505a98c2bad5ea594195f34cc5d
Maximilien <maximilien.lemoine.pro@gmail.com>
2026-04-04 14:26:17 +0200

    choree: mise à jour des libs avril

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,10 @@ Encore
      * https://symfony.com/doc/current/frontend.html#adding-more-features
      */
     .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
+
+    // Displays build status system notifications to the user
+    // .enableBuildNotifications()
+
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
@@ -50,7 +53,7 @@ Encore
     // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
-        config.corejs = '3.23';
+        config.corejs = '3.38';
     })
 
     // uncomment if you use TypeScript
